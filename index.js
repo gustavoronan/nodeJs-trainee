208298f1d59386dc1fc827bb0aa45bd3ce4415ba
gustavo <gustavoronn@gmail.com>
2024-12-05 10:43:04 -0300

    mais funcionalidades adicionadas ao servidor

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-const express = require('express')
 
-console.log(express)
+//Query params = ?nome=NodeJS
+//Route params = /curso/:id
+//Request Body = { nome: 'NodeJS', tipo:'Backend' }
 
-const server = express()
 
-server.get('/curso', (req, res)=>{
-    return res.json({curso: "Hello World"})
+const express = require('express') //recebendo a ferramenta express e armazenando dentro de uma const chamada express
+
+console.log(express) //verificando se foi instalado com sucesso
+
+const server = express() //criando o servidor com express
+
+const cursos = ['NodeJS', 'Java', 'PHP']
+
+server.get('/curso', (req, res)=>{ //passando a rota /curso, e a funcao que deve ser executada ao acessar a rota
+    const nome = req.query.nome // passando para a vareavel a requisicao + query
+    return res.json({curso: `aprendendo ${nome}`}) //retorno de um json no frontend
 })
 
-server.listen(3000)
+
+server.get('/route/:id', (req, res)=>{ //passando a rota /route um id a ser acessado
+    const id = req.params.id //funciona da mesma forma que o query params
+    return res.json({curso: `Curso ${id}`}) //retorno de um json no frontend
+})
+
+server.get('/cursos/:index', (req, res)=>{ //passando a rota /route um index como requisicao
+    const { index } = req.params //recebendo a requisicao e armazenando na vareavel index, de uma forma a usar desentruturação, basicamente acessando o index
+    //de uma forma direta pelo req.params, basicamente dizendo: Do objeto req.params, pegue a propriedade chamada index e guarde seu valor na variável index
+    return res.json(cursos[index]) //retorno de um json no frontend
+})
+
+
+server.listen(3000) //porta que o servidor esta rodando
